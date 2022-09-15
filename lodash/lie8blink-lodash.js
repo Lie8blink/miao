0463f119fcf2583742a0630dd1d444c058292904
Lie8blink <1913858722@qq.com>
2022-09-15 20:43:08 +0800

    9/15

--- lodash/lie8blink-lodash.js
+++ lodash/lie8blink-lodash.js
@@ -46,53 +46,221 @@ var lie8blink = {
         return ary;
     },
 
-    fill:function(array, value, start=0, end=array.length){
-        for(start;start<end;start++){
-            array[start]=value
+    fill: function (array, value, start = 0, end = array.length) {
+        for (start; start < end; start++) {
+            array[start] = value
         }
         return array
     },
 
-    drop:function(array, n=1){
-        const len=array.length
-        const result=[]
-        for(n;n<len;n++){
+    drop: function (array, n = 1) {
+        const len = array.length
+        const result = []
+        for (n; n < len; n++) {
             result.push(array[n])
         }
         return result
     },
 
-    // findIndex:function(array, predicate=_.identity, fromIndex=0){
+    findIndex: function (array, predicate = _.identity, fromIndex = 0) {
 
-    // },
+    },
 
-    // findLastIndex: function() {},
+    findLastIndex: function () { },
 
-    // flatten: function() {},
+    flatten: function (array) {
+        const result = []
+        for (let i = 0; i < array.length; i++) {
+            let x = array[i]
+            if (typeof x === "object") {
+                for (let j = 0; j < x.length; j++) {
+                    result.push(x[j])
+                }
+            } else {
+                result.push(x)
+            }
+        }
+        return result
+    },
 
-    // flattenDeep: function() {},
+    flattenDeep: function (array) {
+        const result = []
+        while (flag = true) {
+            let flag = true
+            for (let i = 0; i < array.length; i++) {
+                let x = array[i]
+                if (typeof x === "object") {
+                    flag = false
+                    for (let j = 0; j < x.length; j++) {
+                        result.push(x[j])
+                    }
+                } else {
+                    result.push(x)
+                }
+            }
+        }
+        return result
+    },
 
-    // flattenDepth: function() {},
+    flattenDepth: function (array, depth = 1) {
+        const result = []
+        for (depth; depth > 0; depth--) {
+            result = flatten(array)
+        }
+        return result
+    },
 
-    // fromPairs: function() {},
+    fromPairs: function (pairs) {
+        const result = {}
+        for (let i = 0; i < pairs.length; i++) {
 
-    // head: function() {},
+        }
+    },
 
-    // indexOf: function() {},
+    head: function (array) {
+        return array[0]
+    },
 
-    // lastIndexOf: function() {},
+    indexOf: function (array, value, fromIndex = 0) {
+        for (let i = fromIndex; i < array.length; i++) {
+            if (array[i] === value) {
+                return i
+            }
+        }
+        return -1
+    },
 
-    // initial: function() {},
+    lastIndexOf: function (array, value, fromIndex = array.length - 1) {
+        for (let i = fromIndex; i >= 0; i--) {
+            if (array[i] === value) {
+                return i
+            }
+        }
+        return -1
+    },
 
-    // join: function() {},
+    initial: function (array) {
+        let result = array
+        if (result.length >= 1) result.length -= 1;
+        return result
+    },
 
-    // last: function() {},
+    join: function (array, separator = ',') {
+        if (array.length === 1) return array[0]
+        let result = ""
+        for (let i = 0; i < array.length; i++) {
+            if (i == 0) {
+                result = array[i]
+            } else {
+                result = result + separator + array[i]
+            }
+        }
+        return result
+    },
 
-    // pull: function() {},
+    last: function (array) {
+        return array[array.length - 1]
+    },
 
-    // reverse: function() {},
+    pull: function (array, values) {
+        let result = []
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] !== values) {
+                result.push(array[i])
+            }
+        }
+        return result
+    },
 
-    // every: function() {},
+    reverse: function (array) {
+        let result = []
+        for (let i = array.length - 1; i >= 0; i--) {
+            result.push(array[i])
+        }
+        return result
+    },
 
-    // some: function() {},
+    every: function () {
+
+    },
+
+    some: function () {
+
+    },
+
+    countBy: function () {
+
+    },
+
+    groupBy: function () {
+
+    },
+
+    keyBy: function () {
+
+    },
+
+    forEach: function () {
+
+    },
+
+    map: function () {
+
+    },
+
+    filter: function () {
+
+    },
+
+    reduce: function () {
+
+    },
+
+    reduceRight: function () {
+
+    },
+
+    size: function () {
+
+    },
+
+    sortBy: function () {
+
+    },
+
+    sample: function () {
+
+    },
+
+    // isUndefined,
+    // isNull,
+    // isNil,
+    // max,
+    // min,
+    // maxBy,
+    // minBy,
+    // round,
+    // sumBy
+    // flagMap,
+    // flatMapDepth,
+    // get,
+    // has,
+    // mapKeys,
+    // mapValues
+    // range,
+    // stringifyJSON,
+    // concat,isEqual,
+    // repeat,
+    // padStart,
+    // padEnd,
+    // pad,
+    // keys,
+    // random,
+    // round,
+    // ceil,
+    // floor,
+    // cloneDeep
+    // trim,
+    // trimStart,
+    // trimEnd
 }
